841ada590432fcf0594d6032c318f92fd46c42f3
Skyler Harris <skyler.iqeq@gmail.com>
2021-10-26 20:35:09 +0800

    rewrite if else into ? task

---
+++ conditional_branching_if/rewrite_if_else_into.js
@@ -0,0 +1,21 @@
+// Rewrite if..else using multiple ternary operators '?'
+// For redability, it's recommended to split the code into multiple lines
+
+let message;
+
+if (login == 'Employee') {
+    message = 'Hello';
+} else if (login == 'Director') {
+    message = 'Greetings';
+} else if (login == '') {
+    message = 'No login';
+} else {
+    message = '';
+}
+
+
+
+let message = ( login == 'Employee' ) ? 'Hello' : 
+    ( login == 'Director' ) ? 'Greetings' :
+    ( login == '') ? 'No login' :
+    '';
