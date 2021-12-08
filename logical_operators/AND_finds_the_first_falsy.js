4e605a52267a487bb3ac0e4548df172a746f3a84
SkyCHarris <skyler.iqeq@gmail.com>
2021-12-09 01:30:06 +0800

    AND returns the first falsy value

---
+++ logical_operators/AND_finds_the_first_falsy.js
@@ -0,0 +1,12 @@
+
+// if the first operand is truthy,
+// AND returns the second operand:
+
+alert( 1 && 0 ); // 0
+alert( 1 && 5 ); // 5
+
+// if the first oeprand is falsy,
+// AND returns it. The second operand is ignored
+
+alert( null && 5 ); // null
+alert( 0 && "no matter what" ); // 0
