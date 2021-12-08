a88b438cdc6e19b8e7cf3232c2b82d64fe512dba
SkyCHarris <skyler.iqeq@gmail.com>
2021-12-09 01:23:05 +0800

    AND

--- logical_operators/AND.js
+++ logical_operators/AND.js
@@ -3,3 +3,10 @@ alert( true && true ); // true
 alert( false && false ); // false
 alert( true && false ); // false
 alert( false && false ); // false
+
+let hour = 12;
+let minute = 30;
+
+if (hour == 12 && minute == 30) {
+    alert( 'The time is 12:30' );
+}
