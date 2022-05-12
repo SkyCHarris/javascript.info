87842976f8d75233ba533c7a46ea3b0f8f687181
Skyler Harris <skyler.iqeq@gmail.com>
2022-05-12 10:14:31 +0800

    checked the login first half

--- logical_operators/a_question_about_if.js
+++ logical_operators/a_question_about_if.js
@@ -1,5 +1,5 @@
 // What will the results of the expressions be inside if(...)?
 
-if (-1 || 0) alert( 'first' );
-if (-1 && 0) alert( 'second' );
-if (null || -1 && 1) alert( 'third' );
+if (-1 || 0) alert( 'first' ); // executes
+if (-1 && 0) alert( 'second' ); 
+if (null || -1 && 1) alert( 'third' ); // executes
---
+++ logical_operators/check_the_login.js
@@ -0,0 +1,9 @@
+let visitor = prompt("Login");
+
+if ( visitor = "Admin") {
+    prompt("Password")
+} else if ( visitor = undefined ) {
+    alert( "Canceled" )
+} else ( visitor = "") {
+    alert( "I don't know you")
+}
