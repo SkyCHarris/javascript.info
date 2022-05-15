48106e7e79d61e83d83e19c1ebf651fcc45dffc8
Skyler Harris <skyler.iqeq@gmail.com>
2022-05-15 18:53:07 +0800

    for loops tutorial

--- loops_while_and_for/tutorial.js
+++ loops_while_and_for/tutorial.js
@@ -1,31 +1,55 @@
 // The "while" loop
 
-// while (condition) {
-//     // code
-//     // so-called "loop body"
-// }
+while (condition) {
+    // code
+    // so-called "loop body"
+}
 
-// let i = 0;
-// while (i < 3) { // shows 0, then 1, then 2
-//     alert( i );
-//     i++;
-// }
+let i = 0;
+while (i < 3) { // shows 0, then 1, then 2
+    alert( i );
+    i++;
+}
 
-// let i = 3;
-// while (i) { // when i becomes 0, the condition becomes falsy, and the loop stops
-//     alert( i );
-//     i--;
-// }
+let i = 3;
+while (i) { // when i becomes 0, the condition becomes falsy, and the loop stops
+    alert( i );
+    i--;
+}
 
 
 // The "do...while" loop
 
-// do {
-//     // loop body
-// } while (condition);
+do {
+    // loop body
+} while (condition);
 
 let i = 0;
 do {
     alert( i );
     i++;
 } while (i < 3);
+
+
+// The "for" loop
+
+for (begin; condition; step) {
+    // ... loop body ...
+}
+
+for (let i = 0; i <3; i++) { // shows 0, then 1, then 2
+    alert(i);
+}
+
+// for (let i=0; i<3; i++)
+
+// run begin
+let i = 0
+// if condition -> run body and run step
+if (i <3) { alert(i); i++}
+// if condition -> run body and run step
+if (i <3) { alert(i); i++}
+// if condition -> run body and run step
+if (i <3) { alert(i); i++}
+// ...finish, because now i == 3
+
