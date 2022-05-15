67bae4d2da6322e16e90ff05c1ba58467ec80e51
Skyler Harris <skyler.iqeq@gmail.com>
2022-05-15 18:44:30 +0800

    loops while and for tutorial while and do while loops

---
+++ loops_while_and_for/tutorial.js
@@ -0,0 +1,31 @@
+// The "while" loop
+
+// while (condition) {
+//     // code
+//     // so-called "loop body"
+// }
+
+// let i = 0;
+// while (i < 3) { // shows 0, then 1, then 2
+//     alert( i );
+//     i++;
+// }
+
+// let i = 3;
+// while (i) { // when i becomes 0, the condition becomes falsy, and the loop stops
+//     alert( i );
+//     i--;
+// }
+
+
+// The "do...while" loop
+
+// do {
+//     // loop body
+// } while (condition);
+
+let i = 0;
+do {
+    alert( i );
+    i++;
+} while (i < 3);
