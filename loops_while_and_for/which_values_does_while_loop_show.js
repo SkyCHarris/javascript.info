6e836e42e886c021fa84ed3ce70aeedff7d27d03
Skyler Harris <skyler.iqeq@gmail.com>
2022-05-16 22:23:39 +0800

    which values does while loop show

---
+++ loops_while_and_for/which_values_does_while_loop_show.js
@@ -0,0 +1,17 @@
+// For every loop iteration, write down which value it outputs and then compare it with the solution.
+// Both loops alerty the same values, or not?
+
+// 1. The prefix form ++i
+
+let i = 0;
+while (++i < 5) alert( i );
+
+// 1, 2, 3, 4
+
+
+// 2. The postfix form i++
+
+let i = 0;
+while (i++ < 5) alert( i );
+
+// 1, 2, 3, 4, 5
