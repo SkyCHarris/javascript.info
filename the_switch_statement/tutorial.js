c11b6585927a6445c6763195ba7dce78385b826a
Skyler Harris <skyler.iqeq@gmail.com>
2022-06-01 13:48:57 +0800

    type matters tutorial

--- the_switch_statement/tutorial.js
+++ the_switch_statement/tutorial.js
@@ -42,3 +42,43 @@ switch (+a) {
         default:
             alert("this doesn't run");
 }
+
+// Grouping of "case"
+
+let a = 3;
+
+switch (a) {
+    case 4:
+        alert('Right!');
+        break;
+
+    case 3: // (*) grouped two cases
+    case 5:
+        alert('Wrong!');
+        alert("Why don't you take a math class?");
+        break;
+
+        default:
+            alert('The result is strange. Really.')
+}
+
+
+// Type matters
+
+let arg = prompt("Enter a value?");
+switch (arg) {
+    case '0':
+    case '1':
+        alert( 'One or zero' );
+        break;
+
+    case '2':
+        alert( 'Two' );
+        break;
+
+    case 3:
+        alert( 'Never executes!' );
+        break;
+    default:
+        alert( 'An unkown value' );
+}
